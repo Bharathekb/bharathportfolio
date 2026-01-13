baa469224bda35c51a216afc513fa557db9a90f5
Bharath ekb <bharathblog9502@gmail.com>
2026-01-13 19:08:38 +0530

    Updated app component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
     <div className="Main-Box ">
       <div className="container-fluid My-container-fluid p-0">
         <div className="Main-container d-flex">
- <Sidebar />
-        <div className="Right-main flex-grow-1 p-4">
-          <Dashboard />
+          <Sidebar />
+          <div className="Right-main flex-grow-1 p-4">
+            <Dashboard />
+          </div>
         </div>
-        </div>
-       
+
       </div>
 
     </div>
